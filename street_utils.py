1d97671781d5827f5e908077f3429a8afd04dd70
Ubuntu <ubuntu@ip-172-31-47-99.ec2.internal>
2015-09-30 01:03:02 +0000

    More utils for data preparation

--- street_utils.py
+++ street_utils.py
@@ -6,19 +6,46 @@ import os
 import pdb
 import subprocess
 
+def _mkdir(fName):
+	if not osp.exists(fName):
+		os.makedirs(fName)
+
 def process_folder():
 	pass
 
+##
+# Helper function for get_foldernames
+def _find_im_labels(folder):
+	fNames = os.listdir(folder)
+	fNames = [f.strip() for f in fNames]
+	if len(fNames)==1:
+		folder = osp.join(folder, fNames[0])
+		print (folder)
+		return _find_im_labels(folder)
+	return folder
+
+##
+# Find the foldernames in which data is stored
 def get_foldernames(prms):
 	'''
 		Search for the folder tree - till the time there are no more
 		directories. We will assume that the terminal folder has all
 	  the needed files
 	'''
-	fNames = osp.listdir(prms.paths.rawData)
-	fNames = [f for f in fNames if osp.isdir(f)]
-	
+	fNames = [f.strip() for f in os.listdir(prms.paths.raw.dr)]
+	fNames = [osp.join(prms.paths.raw.dr, f) for f in fNames]
+	print (fNames)
+	fNames = [_find_im_labels(f) for f in fNames if osp.isdir(f)]
+	return fNames
 
+##
+# Save the foldernames along with the folder ids
+def save_foldernames(prms):	
+	fNames = get_foldernames(prms)
+	fid    = open(prms.paths.proc.folders.key, 'w')
+	for (i,f) in enumerate(fNames):
+		fid.write('%04d \t %s\n' % (i,f))
+	fid.close()
 
 def get_tar_files(prms):
 	with open(prms.paths.tarListFile,'r') as f:
@@ -43,12 +70,26 @@ def get_paths():
 	#For storing the directories
 	paths.dirs = edict()
 	#The raw data
-	paths.dirs.data     = '/data1/pulkitag/data_sets/streetview'
-	paths.dirs.rawData  = osp.join(paths.dirs.data, 'raw')
-	paths.dirs.procData = osp.join(paths.dirs.data, 'proc')
-	paths.dirs.tarData  = osp.join(paths.dirs.data, 'tar')
-	paths.dirs.code     = '/home/ubuntu/code/streetview'
-	paths.tarListFile   = osp.join(paths.dirs.code, 'data_list.txt') 
+	paths.dataDr  = '/data1/pulkitag/data_sets/streetview'
+	paths.raw     = edict()
+	paths.raw.dr  = osp.join(paths.dataDr, 'raw')
+	#Processed data
+	paths.proc    = edict()
+	paths.proc.dr = osp.join(paths.dataDr, 'proc')
+	#Raw Tar Files
+	paths.tar     = edict()
+	paths.tar.dr  = osp.join(paths.dataDr, 'tar')
+	#The Code directory
+	paths.code    = edict()
+	paths.code.dr = '/home/ubuntu/code/streetview'
+	#List of tar files
+	paths.tar.fileList = osp.join(paths.code.dr, 'data_list.txt') 
+	#Store the names of all the folders
+	paths.proc.folders    = edict()
+	paths.proc.folders.dr = osp.join(paths.proc.dr, 'folders')
+	_mkdir(paths.proc.folders.dr)
+	#Stores the folder names along with the keys
+	paths.proc.folders.key = osp.join(paths.proc.folders.dr, 'key.txt') 
 	return paths
 
 
