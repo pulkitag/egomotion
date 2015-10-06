ee308893643cbad69d866280ca6271954028fccc
Ubuntu <ubuntu@ip-172-31-47-99.ec2.internal>
2015-10-06 08:08:59 +0000

    Adding base files for net definitions

--- street_exp.py
+++ street_exp.py
@@ -23,13 +23,24 @@ def get_nw_prms(**kwargs):
 # Parameters that specify the learning
 def get_lr_prms(**kwargs):	
 	dArgs = edict()
-	dArgs.batchSz = 128
-	dArgs.stepSz  = 20000	
+	dArgs.batchsize = 128
+	dArgs.stepsize  = 20000	
+	dArgs.base_lr   = 0.001
+	dArgs.max_iter  = 250000
+	dArgs.gamma     = 0.5
+	dArgs.weight_decay = 0.0005 
 	dArgs  = mpu.get_defaults(kwargs, dArgs)
-	expStr = 'batchSz%d_stepSz%d' % (dArgs.batchSz, dArgs.stepSz)
-	dArgs.expStr = expStr 
-	
- 	return dArgs 
+	expStr = 'batchSz%d_stepSz%d_blr%.5f_mxItr%.2e_gamma%.2f_wdecay%.6f'\
+					 % (dArgs.batchsize, dArgs.stepsize, dArgs.base_lr,
+							dArgs.max_iter, dArgs.gamma, dArgs.weight_decay)
+	dArgs.expStr = expStr
+	#Make the solver 
+	solArgs = edict({'test_iter': 100, 'test_interval': 1000,
+						 'snapshot': 1000, 'debug_info': 'true'})
+	for k in dArgs.keys():
+		solArgs[k] = copy.deepcopy(dArgs[k])
+	dArgs.solver = mpu.make_solver(**solArgs)	
+	return dArgs 
 
 ##
 # Parameters for fine-tuning
@@ -58,7 +69,6 @@ def get_finetune_prms(**kwargs):
 
 
 def get_caffe_prms(nwPrms, lrPrms, finePrms=None, isScratch=True, deviceId=1): 
-									
 	caffePrms = edict()
 	caffePrms.deviceId  = deviceId
 	caffePrms.isScratch = isScratch
@@ -66,72 +76,99 @@ def get_caffe_prms(nwPrms, lrPrms, finePrms=None, isScratch=True, deviceId=1):
 	caffePrms.lrPrms    = copy.deepcopy(lrPrms)
 	caffePrms.finePrms  = copy.deepcopy(finePrms)
 
-	expStr = []
-	expStr.append('con-%s' % concatLayer)
-	if isScratch:
-		expStr.append('scratch')
-	if concatDrop:
-		expStr.append('con-drop')
-	expStr.append('pad%d' % contextPad)
-	expStr.append('imS%d' % imSz)	
-
-	if convConcat:
-		expStr.append('con-conv')
-	
-	if isMySimple:
-		expStr.append('mysimple')
-
-	if contrastiveMargin is not None:
-		expStr.append('ct-margin%d' % contrastiveMargin)
-
-	if isFineTune:
-		if fineDataSet=='sun':
-			assert (fineMaxIter is None) and (stepsize is None)
-			#These will be done automatically.
-			if imSz==227 or imSz==256:
-				sunImSz = 256
-				muFile = '"%s"' % '/data1/pulkitag/caffe_models/ilsvrc2012_mean.binaryproto'
-			else:
-				sunImSz = 128
-				muFile = '"%s"' % '/data1/pulkitag/caffe_models/ilsvrc2012_mean_imSz128.binaryproto'
-			caffePrms['fine']['muFile'] = muFile
-			print "Using mean from: ", muFile 
-			sunPrms     = ps.get_prms(numTrainPerClass=fineNumData, runNum=fineRunNum, imSz=sunImSz)
-			numCl       = 397
-			numTrainEx  = numCl * fineNumData  
-			maxEpoch    = 30
-			numSteps    = 2
-			epochIter   = np.ceil(float(numTrainEx)/batchSz)
-			caffePrms['stepsize'] = int(maxEpoch * epochIter)
-			caffePrms['fine']['max_iter'] = int(numSteps * caffePrms['stepsize']) 
-			caffePrms['fine']['gamma'] = 0.1
-			caffePrms['fine']['prms']  = sunPrms
-			expStr.append('small-data')
-
-		expStr.append(fineDataSet)
-		if sourceModelIter is not None:
-			expStr.append('mItr%dK' % int(sourceModelIter/1000))
-		else:
-			expStr.append('scratch')	
-		if lrAbove is not None:
-				expStr.append('lrAbv-%s' % lrAbove)
-		expStr.append('bLr%.0e' % fine_base_lr)
-		expStr.append('run%d' % fineRunNum)
-		expStr.append('datN%.0e' % fineNumData)
-		if fineMaxLayer is not None:
-			expStr.append('mxl-%s' % fineMaxLayer)
-		if addDrop:
-			expStr.append('drop')
-		if extraFc:
-			expStr.append('exFC')
-		if imgntMean:
-			expStr.append('muImgnt')
-
-	expStr = ''.join(s + '_' for s in expStr)
-	expStr = expStr[0:-1]
+	expStr = nwPrms.expStr + '/' + lrPrms.expStr
+	if finePrms is not None:
+		expStr = expStr + '/' + finePrms.expStr
 	caffePrms['expStr'] = expStr
-	caffePrms['solver'] = get_solver(caffePrms, isFine=isFineTune)
+	caffePrms['solver'] = lrPrms.solver
 	return caffePrms
+
+def setup_experiment(prms, cPrms):
+	#The size of the labels
+	if prms['pose'] == 'euler':
+		rotSz = 3
+		trnSz = 3
+	elif prms['pose'] == 'sigMotion':
+		rotSz = 1
+		trnSz = 2
+	elif prms['pose'] == 'rotOnly':
+		rotSz = 3
+		trnSz = 0
+	elif prms['pose'] == 'slowness':
+		pass
+	else:
+		raise Exception('Unrecognized %s pose type' % prms['pose'])
+
+	#The base file to start with
+	baseFileStr  = 'kitti_siamese_window_%s' % cPrms['concatLayer']
+	if prms['lossType'] == 'classify':
+		baseStr = '_cls-trn%d-rot%d' % (trnSz, rotSz)
+		if cPrms['convConcat']:
+			baseStr = baseStr + '_concat_conv'
+		if cPrms['isMySimple']:
+			baseStr = baseStr + '_mysimple'
+	elif prms['lossType'] in ['contrastive']:
+		baseStr =  '_%s' % prms['lossType']
+	else:
+		baseStr = ''
+	baseFile = os.path.join(baseFilePath, baseFileStr + baseStr + '.prototxt')
+	print baseFile
+
+	protoDef = mpu.ProtoDef(baseFile)	 
+	solDef   = cPrms['solver']
+	
+	caffeExp = get_experiment_object(prms, cPrms)
+	caffeExp.init_from_external(solDef, protoDef)
+
+	#Get the source file for the train and test layers
+	caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'source'],
+			'"%s"' % prms['paths']['windowFile']['train'], phase='TRAIN')
+	caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'source'],
+			'"%s"' % prms['paths']['windowFile']['test'], phase='TEST')
+
+	#Set the root folder
+	caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'root_folder'],
+			'"%s"' % prms['paths']['imRootDir'], phase='TRAIN')
+	caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'root_folder'],
+			'"%s"' % prms['paths']['imRootDir'], phase='TEST')
+
+	if prms['randomCrop']:
+		caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'random_crop'],
+			'true', phase='TRAIN')
+		caffeExp.set_layer_property('window_data', ['generic_window_data_param', 'random_crop'],
+			'true', phase='TEST')
+	
+
+	if prms['lossType'] == 'classify':
+		for t in range(trnSz):
+			caffeExp.set_layer_property('translation_fc_%d' % (t+1), ['inner_product_param', 'num_output'],
+									prms['binCount'], phase='TRAIN')
+		for r in range(rotSz):
+			caffeExp.set_layer_property('rotation_fc_%d' % (r+1), ['inner_product_param', 'num_output'],
+									prms['binCount'], phase='TRAIN')
+	elif prms['lossType'] == 'contrastive':
+		caffeExp.set_layer_property('loss', ['contrastive_loss_param', 'margin'],
+								cPrms['contrastiveMargin'])
+	else:
+		#Regression loss basically
+		#Set the size of the rotation and translation layers
+		caffeExp.set_layer_property('translation_fc', ['inner_product_param', 'num_output'],
+								trnSz, phase='TRAIN')
+		caffeExp.set_layer_property('rotation_fc', ['inner_product_param', 'num_output'],
+								rotSz, phase='TRAIN')
+
+	if prms['lossType'] in ['contrastive']:
+		pass
+	else:
+		#Decide the slice point for the label
+		#The slice point is decided by the translation labels.
+		if trnSz == 0:
+			slcPt = 1
+		else:
+			slcPt = trnSz	
+		caffeExp.set_layer_property('slice_label', ['slice_param', 'slice_point'], slcPt)	
+	return caffeExp
+
 
 
 def get_experiment_object(prms, cPrms):
