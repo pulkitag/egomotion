775a1fa831580e1b51fb20c61353bc7cfdb103a8
Ubuntu <ubuntu@ip-172-31-47-99.ec2.internal>
2015-10-11 01:28:51 +0000

    Stuff Happening

--- street_exp.py
+++ street_exp.py
@@ -37,7 +37,7 @@ def get_lr_prms(**kwargs):
 	dArgs  = mpu.get_defaults(kwargs, dArgs)
 	#Make the solver 
 	solArgs = edict({'test_iter': 100, 'test_interval': 1000,
-						 'snapshot': 1000, 'debug_info': 'false'})
+						 'snapshot': 1000, 'debug_info': 'true'})
 	for k in dArgs.keys():
 		if k in ['batchsize']:
 			continue
