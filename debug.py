1ccf8361783ad9450dcbffcd10dd78c690855b7b
pulkitag <pulkitag@berkeley.edu>
2015-10-12 03:08:03 -0700

    Started adding debugging support

---
+++ debug.py
@@ -0,0 +1,50 @@
+## @package debug
+# Debug various aspects of the pipeline
+#
+import street_exp as se
+import os.path as osp
+import street_params as sp
+import caffe
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+
+def debug_generic_data():
+	#Setup the data proto
+	bSz   = 5
+	prms  = sp.get_prms_pose(geoFence='dc-v1')	
+	nPrms = se.get_nw_prms(imSz=101, netName='smallnet-v2',
+								 concatLayer='pool4', maxJitter=0)
+	lPrms = se.get_lr_prms(batchsize=bSz)
+	cPrms = se.get_caffe_prms(nPrms, lPrms) 
+	#Save the data proto
+	outFile  = osp.join(prms.paths.baseNetsDr, 'data_debug.prototxt')
+	dataDef  = se.make_data_proto(prms, cPrms)
+	dataDef.del_layer_property('window_data', 'transform_param', phase='TRAIN')
+	dataDef.del_layer_property('window_data', 'transform_param', phase='TEST')
+	dataDef.write(outFile)
+
+	#Load the data through the layer and save it
+	svDr = osp.join(prms.paths.code.dr, 'debug-data')
+	plt.figure()
+	ax1  = plt.subplot(121)
+	ax2  = plt.subplot(122)
+	net = caffe.Net(outFile, caffe.TEST)
+	N     = 4
+	count = 0
+	for i in range(N):
+		allDat = net.forward(['data', 'data_p'])
+		im1Dat = allDat['data']
+		im2Dat = allDat['data_p']
+		for b in range(bSz):
+			print(i,b)
+			im1 = im1Dat[b].transpose((1,2,0))
+			im2 = im2Dat[b].transpose((1,2,0))
+			im1 = im1[:,:,[2,1,0]]
+			im2 = im2[:,:,[2,1,0]]
+			ax1.imshow(im1.astype(np.uint8))
+			ax2.imshow(im2.astype(np.uint8))
+			svFile = osp.join(svDr, '%04d.pdf' % count)
+			with PdfPages(svFile) as pdf:
+				pdf.savefig()
+			count += 1
--- my_exp.py
+++ my_exp.py
@@ -26,6 +26,19 @@ def smallnetv2_pool4_pose(isRun=False):
 	else:
 		return prms, cPrms	
 
+def smallnetv3_pool4_pose(isRun=False):
+	prms  = sp.get_prms_pose(geoFence='dc-v1')
+	nPrms = se.get_nw_prms(imSz=101, netName='smallnet-v3',
+							 concatLayer='pool4')
+	lPrms = se.get_lr_prms(batchsize=256, stepsize=10000)
+	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=[0,1])
+	if isRun:
+		exp   = se.make_experiment(prms, cPrms)
+		exp.run()
+	else:
+		return prms, cPrms	
+
+
 ##Run the smallnet for pose but with random cropping
 def smallnetv2_pool4_pose_randcrp(isRun=False):
 	prms  = sp.get_prms_pose(geoFence='dc-v1')
