246841b8290b168bcb1f9460248dae21db22ba4e
pulkitag <pulkitag@berkeley.edu>
2015-10-26 20:10:30 -0700

    Support for pose classification

--- my_exp_v2.py
+++ my_exp_v2.py
@@ -36,6 +36,39 @@ def smallnetv2_pool4_pose_euler_mx45_rawImSz256(isRun=False, numTrain=1e+7,
 	return prms, cPrms	
 
 
+def smallnetv2_pool4_pose_euler_mx45_crp192_rawImSz256(isRun=False, numTrain=1e+7, 
+										deviceId=[0], isPythonLayer=False, isGray=False):
+	prms  = sp.get_prms(geoFence='dc-v2', labels=['pose'], labelType=['euler'],
+											lossType=['l2'], maxEulerRot=45, rawImSz=256,
+											splitDist=100, numTrain=numTrain, crpSz=192)
+	nPrms = se.get_nw_prms(imSz=101, netName='smallnet-v2',
+							 concatLayer='pool4', lossWeight=10.0,
+								randCrop=False, concatDrop=False,
+								isGray=isGray, isPythonLayer=isPythonLayer)
+	lPrms = se.get_lr_prms(batchsize=256, stepsize=10000, clip_gradients=10.0)
+	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=deviceId)
+	if isRun:
+		exp   = se.make_experiment(prms, cPrms)
+		exp.run()
+	return prms, cPrms	
+
+
+def smallnetv2_pool4_pose_classify_euler_mx45_crp192_rawImSz256(isRun=False, numTrain=1e+7, 
+										deviceId=[0], isPythonLayer=False, isGray=False):
+	prms  = sp.get_prms(geoFence='dc-v2', labels=['pose'], labelType=['euler'],
+											lossType=['classify'], maxEulerRot=45, rawImSz=256,
+											splitDist=100, numTrain=numTrain, crpSz=192)
+	nPrms = se.get_nw_prms(imSz=101, netName='smallnet-v2',
+							 concatLayer='pool4', lossWeight=10.0,
+								randCrop=False, concatDrop=False,
+								isGray=isGray, isPythonLayer=isPythonLayer)
+	lPrms = se.get_lr_prms(batchsize=256, stepsize=10000, clip_gradients=10.0)
+	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=deviceId)
+	if isRun:
+		exp   = se.make_experiment(prms, cPrms)
+		exp.run()
+	return prms, cPrms	
+
 
 def smallnetv2_pool4_ptch_crp192_rawImSz256(isRun=False, isGray=False, numTrain=1e+7,
 																		isPythonLayer=False, deviceId=[2]):
