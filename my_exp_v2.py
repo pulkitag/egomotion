f11b0ec599fcae56f4eaeb94065ed82332bc9bf7
pulkitag <pulkitag@berkeley.edu>
2015-10-28 06:40:10 -0700

    Local Commit

--- my_exp_v2.py
+++ my_exp_v2.py
@@ -3,7 +3,7 @@ import street_params as sp
 import street_exp as se
 
 def smallnetv2_pool4_pose_crp192_rawImSz256(isRun=False, isGray=False, numTrain=1e+7,
-																						deviceId=[0], isPythonLayer=False):
+																						deviceId=[0], isPythonLayer=False, runNum=0):
 	prms  = sp.get_prms_pose(geoFence='dc-v2', crpSz=192,
 													 rawImSz=256, splitDist=100,
 													 numTrain=numTrain)
@@ -12,7 +12,7 @@ def smallnetv2_pool4_pose_crp192_rawImSz256(isRun=False, isGray=False, numTrain=
 								randCrop=False, concatDrop=False,
 								isGray=isGray, isPythonLayer=isPythonLayer)
 	lPrms = se.get_lr_prms(batchsize=256, stepsize=10000, clip_gradients=1.0)
-	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=deviceId)
+	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=deviceId, runNum=runNum)
 	if isRun:
 		exp   = se.make_experiment(prms, cPrms)
 		exp.run()
