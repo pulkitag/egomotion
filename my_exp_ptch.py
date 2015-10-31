4b6534056473f709d0ab970ff31adff75aca7d10
Pulkit Agrawal <pagrawal@hsw231.localdomain>
2015-10-31 00:31:56 -0700

    Local Commit

--- my_exp_ptch.py
+++ my_exp_ptch.py
@@ -21,3 +21,23 @@ def smallnetv2_pool4_ptch_crp192_rawImSz256(isRun=False, isGray=False, numTrain=
 		exp.run()
 	return prms, cPrms	
 
+
+def matchnet_ptch_crp192_imSz64_rawImSz256(isRun=False, isGray=False, numTrain=1e+7,
+					isPythonLayer=True, deviceId=[2], batchsize=256,
+					resumeIter=0, extraFc=None):
+	prms  = sp.get_prms_ptch(geoFence='dc-v2', crpSz=192,
+													 rawImSz=256, splitDist=100,
+													 numTrain=numTrain)
+	nPrms = se.get_nw_prms(imSz=64, netName='matchnet',
+							 concatLayer='pool5', lossWeight=10.0,
+								randCrop=False, concatDrop=False,
+								isGray=isGray, isPythonLayer=isPythonLayer,
+								extraFc=extraFc)
+	lPrms = se.get_lr_prms(batchsize=batchsize, stepsize=10000, clip_gradients=10.0)
+	cPrms = se.get_caffe_prms(nPrms, lPrms, deviceId=deviceId,
+								resumeIter=resumeIter)
+	if isRun:
+		exp   = se.make_experiment(prms, cPrms)
+		exp.run()
+	return prms, cPrms	
+
