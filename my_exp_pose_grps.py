749b79c204e1ea7174ca3965a2e3a1ff6f4f746c
Pulkit Agrawal <pagrawal@ivb103.localdomain>
2016-03-06 00:42:06 -0800

    Added support for additional experiments

--- my_exp_pose_grps.py
+++ my_exp_pose_grps.py
@@ -23,5 +23,26 @@ def simple_euler_dof2_dcv2_smallnetv5(isRun=False,
 	if isRun:
 		exp.make(deviceId=deviceId)
 		exp.run() 
+	return exp 	 			
+
+
+def simple_euler_dof2_dcv2_doublefcv1(isRun=False,
+         gradClip=10, stepsize=20000, base_lr=0.001,
+         gamma=0.5, deviceId=0):
+	posePrms = slu.PosePrms(maxRot=90, simpleRot=True, dof=2)
+	dPrms   =  sev2.get_data_prms(lbPrms=posePrms)
+	nwFn    = sev2.process_net_prms
+	nwArgs  = {'ncpu': 0, 'baseNetDefProto': 'doublefc-v1_window_siamese_fc6'}
+	solFn   = mec.get_default_solver_prms
+	solArgs = {'dbFile': DEF_DB % 'sol', 'clip_gradients': gradClip,
+             'stepsize': stepsize, 'base_lr':base_lr, 'gamma':gamma}
+	cPrms   = mec.get_caffe_prms(nwFn=nwFn, nwPrms=nwArgs,
+									 solFn=solFn, solPrms=solArgs)
+	exp     = mec.CaffeSolverExperiment(dPrms, cPrms,
+					  netDefFn=sev2.make_net_def, isLog=True)
+	if isRun:
+		exp.make(deviceId=deviceId)
+		exp.run() 
 	return exp 	 				
+
 
