2074a13bc1cdcabb9762a983c7c0ddc71a2ec9fc
pulkitag <pulkitag@berkeley.edu>
2016-03-14 11:21:27 +0000

    setting up nyu

---
+++ setup_normals.py
@@ -0,0 +1,17 @@
+import scipy.io as sio
+import numpy as np
+
+
+
+def blah():
+	pass
+	
+
+def compute_normal_centers(nrmls):
+	'''
+		nrmls: N x 3 where N is the number of points
+	'''
+
+def read_normal_file(fName):
+	dat = sio.loadmat(sio)
+	
